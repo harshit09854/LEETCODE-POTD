b9d19bbaf7e604a8a78a33f6b08522fe54ceadf2
Harshit pokharna <Harshitpokharna24@gmail.com>
2025-07-14 00:54:10 +0530

    2410. Maximum Matching of Players With Trainers

---
+++ 13-07-2025.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int i = 0;
+        int j=0;
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+ 
+        int PandTMatch = 0;
+        while(i<players.size()&& j<trainers.size()){
+            if(players[i]<=trainers[j]){
+                PandTMatch++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+
+
+
+        }
+        return  PandTMatch;
+    }
+};
