efebb8a1dfc89a1abd0f0310a2c1f9dcd7db8c7f
harshit09854 <harshitpokharna24@gmail.com>
2025-07-14 19:49:44 +0530

    Update 13-07-2025.cpp

--- 13-07-2025.cpp
+++ 13-07-2025.cpp
@@ -1,26 +1,25 @@
-// #include <vector>
-// class Solution {
-// public:
-//     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-//         int i = 0;
-//         int j=0;
-//         sort(players.begin(),players.end());
-//         sort(trainers.begin(),trainers.end());
+ #include <vector>
+ class Solution {
+ public:
+     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int i = 0
+        int j=0;
+       sort(players.begin(),players.end());
+       sort(trainers.begin(),trainers.end());
  
-//         int PandTMatch = 0;
-//         while(i<players.size()&& j<trainers.size()){
-//             if(players[i]<=trainers[j]){
-//                 PandTMatch++;
-//                 i++;
-//                 j++;
-//             }
-//             else{
-//                 j++;
-//             }
+      int PandTMatch = 0;
+      while(i<players.size()&& j<trainers.size()){
+      if(players[i]<=trainers[j]){
+            PandTMatch++;
+                i++;
+                 j++;
+            }
+           else{
+               j++;
+           }
 
 
-
-//         }
-//         return  PandTMatch;
-//     }
-// };
+         }
+       return  PandTMatch;
+    }
+};
